7cb0fc8e1c612d85a4f25c0c7bec9e51b84d2a64
Bogdan Petre <bogpetre@gmail.com>
2025-07-06 19:26:56 -0400

    completed figure 5

--- figures/figure3/figure3.m
+++ figures/figure3/figure3.m
@@ -131,12 +131,13 @@ exportgraphics(gcf,'panels/geometric_similarity.png','ContentType','image','Reso
 
 %% compute similarity of geometry and topography
 % We use these statistics in the main text of the results
-sid_ind = repmat(1:size(wuc_md,1)',1,n_roi);
-roi_ind = kron(helmertCoding(1:n_roi),ones(size(wuc_md,1),1));
-zwuc = zscore(wuc_md,[],2);
-cosim_ctx = zscore(cosim,[],2);
-ztsnr = zscore(tsnr,[],2);
-zwi_cosim = zscore(wi_cosim,[],2);
+sid_ind = repmat(1:size(wuc_md,1)',1,length(good_rois));
+roi_ind = kron(helmertCoding(1:length(good_rois)),ones(size(wuc_md,1),1));
+nanzscore = @(x1)((x1 - nanmean(x1,2))./nanstd(x1,0,2));
+zwuc = nanzscore(wuc_md(:,good_rois));
+cosim_ctx = nanzscore(cosim(:,good_rois));
+ztsnr = nanzscore(tsnr(:,good_rois));
+zwi_cosim = nanzscore(wi_cosim(:,good_rois));
 
 % model (within participant) standardized effect of wuc on cosim while
 % controlling for test-retest reliability (wi_cosim), tSNR, and fixed ROI
@@ -488,7 +489,7 @@ sgtitle({'Transmodal representations are similar','but implemented more idiosync
 
 % add ROI legend
 a1 = axes();
-a1.Position = [0.285,0.475,0.2,0.2];
+a1.Position = [0.33,0.56,0.15,0.15];
 a1.Visible = 'off';
 
 overlay = canlab_get_underlay_image;
--- matlab_libraries/neuromaps_corr_interaction_fx.m
+++ matlab_libraries/neuromaps_corr_interaction_fx.m
@@ -36,18 +36,37 @@ function [B, B_95_range, bootstat, Bstd, Bstd_95_range, bootstat_std, cohensD, c
 
     Bstd0 = zeros(size(obs_val1,2),design_vars-2);
     for j = 1:size(obs_val1,2)
-        X = [zscore(xMain), zscore(xMain).*valMain];
+        %X = [zscore(xMain), zscore(xMain).*valMain];
+        X = xMain;
         if ~isempty(varargin)
             for k = 1:length(varargin{1})
                 % deal with confounds
-                this_cfd = [zscore(varargin{1}{k}(:,j)); zscore(varargin{1}{k}(:,j))];
-                this_cfd = [this_cfd, this_cfd.*valMain]; % let confounds have different effects on different metrics
+                this_cfd = [varargin{1}{k}(:,j); varargin{1}{k}(:,j)];
                 X = [X, this_cfd];
             end
         end
-        Y = [zscore(obs_val1(:,j)); zscore(obs_val2(:,j))];
+        Y = [obs_val1(:,j); obs_val2(:,j)];
+
+        good_roi = find(~isnan(Y) & ~any(isnan(X),2));
+        % we want each outcome standardized separately
+        Y = [zscore(Y(good_roi(1:size(obs_val1,1)))); ...
+            zscore(Y(good_roi(size(obs_val1,1)+1:end)))];
+        % The top half of X is identical to the bottom half, so we don't
+        % need to separate zscoring like we do for Y. Note, that we're
+        % also adding interaction effects for confounds here, which allows
+        % for the confound effects to vary by similarity metric.
+        X = [zscore(X(good_roi,:)), zscore(X(good_roi,:)).*valMain(good_roi,:)];
+
         Bstd0(j,:) = ((X'*X)\X'*Y(:))';
     end
+    % In our non-standardized OLS we interleve interactions and main
+    % effects, but due to the nan-check for zscoring above we have all main
+    % effects first followed by all interaction effects. Let's correct this
+    % for consistency
+    p = size(Bstd0,2);
+    resort_order = reshape(1:p,p/2,2)';
+    Bstd0 = Bstd0(:,resort_order(:));
+
     % main effect and intercept are both zero because we z-score each
     % observed map (our Y) individually so they're mean 0 and have no
     % difference in magnitude. We're just testing if one map is more
