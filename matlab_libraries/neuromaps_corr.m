b9dbb133a198f20015f8077f8bf64e025970dbf9
bogpetre <bogpetre@gmail.com>
2025-07-18 11:43:20 -0400

    fixed perm orientation mismatch and switched from psuedo-t to normal pdf

--- matlab_libraries/neuromaps_corr.m
+++ matlab_libraries/neuromaps_corr.m
@@ -12,10 +12,12 @@ function [B, CI, p, effectsize, perm_var, jk_var, nu] = neuromaps_corr(DV, IV, o
     jk = neuromaps_corr_jk(DV, IV, obs_map, varargin{:});
     jk_var = (n-1)/n * sum((jk - mean(jk)).^2);
 
-    rois = repmat((1:p), n, 1);
-    sid = repmat((1:n)', 1, p);
+    rois = repmat((1:p)', 1, n);
+    sid = repmat((1:n), p, 1);
 
-    obs_map = repmat(obs_map', n, 1);
+    DV = DV';
+    IV = IV';
+    obs_map = repmat(obs_map, 1, n);
     perm_map = repmat(perm_map, n, 1);
 
     isgood = find(~isnan(IV) & ~isnan(DV) & ~isnan(obs_map));
@@ -25,8 +27,9 @@ function [B, CI, p, effectsize, perm_var, jk_var, nu] = neuromaps_corr(DV, IV, o
     sid = sid(isgood);
     obs_map = obs_map(isgood);
     
+    % deal with nans in perm maps (the medial wall) through mean imputation
     new_perm_map = zeros(length(isgood),size(perm_map,2));
-    for i = 1:size(perm_map,2)
+    parfor i = 1:size(perm_map,2)
         this_perm_map = perm_map(:,i);
 
         medial_wall = isnan(this_perm_map) & isgood;
@@ -39,8 +42,6 @@ function [B, CI, p, effectsize, perm_var, jk_var, nu] = neuromaps_corr(DV, IV, o
     clear new_perm_map
 
     % center for interpretable interaction coefficients
-    % superfluous if you've already subtracted out subject means. If all
-    % subjects are mean zero, all data is mean zero too
     IV = IV - mean(IV);
     DV = DV - mean(DV);
 
@@ -51,7 +52,7 @@ function [B, CI, p, effectsize, perm_var, jk_var, nu] = neuromaps_corr(DV, IV, o
     if ~isempty(varargin)
         for i = 1:length(varargin{1})
             % deal with confounds
-            normed_arg = zscore(varargin{1}{i},[],2);
+            normed_arg = zscore(varargin{1}{i}');
             X0 = [X0, normed_arg(isgood).*sid];
         end
     end
@@ -112,17 +113,18 @@ function [B, CI, p, effectsize, perm_var, jk_var, nu] = neuromaps_corr(DV, IV, o
     beta = M \ rhs;
 
     B = beta(end);
-
-    
-    % compute df of se using Welch-Satterthaite approximation
-    %nu = (perm_var + jk_var)^2 ./ (jk_var^2/(n-1) + perm_var^2/(m-1));
-    nu = n -1;
     se = sqrt(perm_var + jk_var);
-    t = B/se;
 
-    %CI = icdf('norm', [0.025, 0.975], 0, sqrt(jk_var)) + B;
-    CI = sqrt(jk_var)*icdf('t', [0.025, 0.975], nu) + B;
-    p = 2*tcdf(-abs(t), nu);
+    CI = icdf('norm', [0.025, 0.975], B, sqrt(jk_var));
+
+    z = B/se;
+    p = 2*normcdf(-abs(z));
+
+    p_naive = (sum(abs(perm) > abs(B))+1)/(length(perm)+1);
+
+    if(p_naive > p)
+        warning('Parametric approximation of joint sampling and null distribution is producing a more significant p-value (%0.6f) than the nonparametric null distribution (%0.6f). This is suss.',p, p_naive);
+    end
         
     effectsize = B / sqrt(perm_var + n*jk_var);
 end
@@ -149,14 +151,14 @@ function B = resample(samples, DV, IV, obs_map, varargin)
     assert(size(obs_map,1) == p);
     assert(size(obs_map,2) == 1);
 
-    rois = repmat((1:p), n-1, 1);
-    sid = repmat((1:n-1)', 1, p);
+    rois = repmat((1:p)', 1, n-1);
+    sid = repmat((1:n-1), p, 1);
 
-    obs_map = repmat(obs_map', n-1, 1);
+    obs_map = repmat(obs_map, n-1, 1);
 
     isgood = find(~isnan(obs_map));
-    %IV = IV(isgood);
-    %DV = DV(isgood);
+    IV = IV';
+    DV = DV';
     rois = rois(isgood);
     sid = sid(isgood);
     obs_map = obs_map(isgood);
@@ -168,8 +170,8 @@ function B = resample(samples, DV, IV, obs_map, varargin)
     % reuse everything avove when we resample
     B = nan(size(samples,2),1);
     parfor i = 1:size(samples,2)
-        this_IV = IV(samples(:,i),:);
-        this_DV = DV(samples(:,i),:);
+        this_IV = IV(:,samples(:,i));
+        this_DV = DV(:,samples(:,i));
 
         this_IV = this_IV(isgood);
         this_DV = this_DV(isgood);
@@ -185,8 +187,8 @@ function B = resample(samples, DV, IV, obs_map, varargin)
         if ~isempty(varargin)
             for j = 1:length(varargin{1})
                 % deal with confounds
-                this_arg = varargin{1}{j}(samples(:,i),:);
-                normed_arg = zscore(this_arg,[],2);
+                this_arg = varargin{1}{j}(samples(:,i),:)';
+                normed_arg = zscore(this_arg);
                 X0 = [X0, normed_arg(isgood).*sid];
             end
         end
