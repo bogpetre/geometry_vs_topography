4978104d724468f9146eb678909f14199fe027df
bogpetre <bogpetre@gmail.com>
2026-02-10 12:01:25 -0500

    sorted clf labels for predictable output order

--- src/geometry_vs_topography/nearestcentroidclf/model.py
+++ src/geometry_vs_topography/nearestcentroidclf/model.py
@@ -91,7 +91,7 @@ def get_clf_mat(model, cv, scorer,
         if np.all(np.isnan(X)):
             continue
 
-        y_set = list(dict.fromkeys(y_label))
+        y_set = sorted(list(dict.fromkeys(y_label)))
         for i,cond1 in enumerate(y_set):
             for cond2 in y_set[(i+1):]:
                 cond_ind = np.array([j for j,lbl in enumerate(y_label) if cond1 == lbl or cond2 == lbl], dtype=int)
