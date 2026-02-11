a8ccfcb9ed7e30b55822c7c89bfda5c787f785ae
bogpetre <bogpetre@gmail.com>
2026-02-11 12:52:24 -0500

    Added missing function reshape_X_to_data()

--- src/geometry_vs_topography/utils/misc.py
+++ src/geometry_vs_topography/utils/misc.py
@@ -1,5 +1,22 @@
 import numpy as np
 
+def reshape_X_to_data(X, X_nodes, data_nodes) -> np.ndarray:
+    # map to data space
+    # 1) mask with data
+    ind = np.array([i for i,s in enumerate(X_nodes) if s in data_nodes])
+    X = X[...,ind]
+    X_nodes = X_nodes[ind]
+    # 2) expand to data space missing from roi space
+    dn_to_Xn = np.vstack([np.array([i, np.where(X_nodes == n)[0][0]], dtype=int)
+                             for i,n in enumerate(data_nodes) if n in X_nodes])
+    if len(X.shape) > 1:
+        X_data = np.zeros((X.shape[0], len(data_nodes)))
+    else:
+        X_data = np.zeros(len(data_nodes))
+    X_data[..., dn_to_Xn[...,0]] = X[...,dn_to_Xn[...,1]]
+    
+    return X_data
+
 def rois_to_data_ind(rois, roi_nodes, data_nodes) -> list:
     '''
     Converts a set of masks defined by unique array values in <rois> into a list of 
